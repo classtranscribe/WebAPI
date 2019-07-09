d67dcbe3c23e6c4c35dd2ec64db493c190e84592
Chirantan Mahipal <chirantan31@gmail.com>
2019-07-09 00:17:21 -0500

    MSTranscription

---
+++ TaskEngine/MSTranscription/MSTranscriptionService.cs
@@ -0,0 +1,112 @@
+﻿using ClassTranscribeDatabase;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskEngine.MSTranscription
+{
+    public class MSTranscriptionService
+    {
+        private AppSettings _appSettings;
+        private SpeechConfig _speechConfig;
+        public MSTranscriptionService(IOptions<AppSettings> appSettings)
+        {
+            _appSettings = appSettings.Value;
+
+            _speechConfig = SpeechConfig.FromSubscription(_appSettings.AZURE_SUBSCRIPTION_KEY, _appSettings.AZURE_REGION);
+        }
+        public async Task<string> RecognitionWithAudioStreamAsync(string file)
+        {
+            // <recognitionAudioStream>
+            // Creates an instance of a speech config with specified subscription key and service region.
+            // Replace with your own subscription key and service region (e.g., "westus").
+            string vttFile = "";
+            var stopRecognition = new TaskCompletionSource<int>();
+            bool fileWritten = false;
+            // Create an audio stream from a wav file.
+            // Replace with your own audio file name.
+            using (var audioInput = Helper.OpenWavFile(file))
+            {
+                // Creates a speech recognizer using audio stream input.
+                using (var recognizer = new SpeechRecognizer(_speechConfig, audioInput))
+                {
+                    // Subscribes to events.
+                    //recognizer.Recognizing += (s, e) =>
+                    //{
+                    //    Console.WriteLine($"RECOGNIZING: Text={e.Result.Text}");
+                    //};
+                    List<Sub> subs = new List<Sub>();
+                    recognizer.Recognized += (s, e) =>
+                    {
+                        if (e.Result.Reason == ResultReason.RecognizedSpeech)
+                        {
+                            TimeSpan offset = new TimeSpan(e.Result.OffsetInTicks);
+                            Console.WriteLine($"Begin={offset.Minutes}:{offset.Seconds},{offset.Milliseconds}", offset);
+                            // Console.WriteLine($"Begin={offset.Minutes}:{offset.Seconds},{offset.Milliseconds}");
+                            TimeSpan end = e.Result.Duration.Add(offset);
+                            Console.WriteLine($"End={end.Minutes}:{end.Seconds},{end.Milliseconds}");
+                            // Console.WriteLine($"Begin={offset.Minutes}:{offset.Seconds},{offset.Milliseconds}");
+                            subs.AddRange(Sub.GetSubs(new Sub
+                            {
+                                Begin = offset,
+                                End = end,
+                                Caption = e.Result.Text
+                            }));
+                        }
+                        else if (e.Result.Reason == ResultReason.NoMatch)
+                        {
+                            Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                        }
+                    };
+
+                    recognizer.Canceled += (s, e) =>
+                    {
+                        Console.WriteLine($"CANCELED: Reason={e.Reason}");
+
+                        if (e.Reason == CancellationReason.Error)
+                        {
+                            Console.WriteLine($"CANCELED: ErrorDetails={e.ErrorDetails}");
+                        }
+                        stopRecognition.TrySetResult(0);
+                        if (!fileWritten)
+                        {
+                            Sub.GenerateSrtFile(subs, file);
+                            Sub.GenerateWebVTTFile(subs, file);
+                        }
+                        fileWritten = true;
+                    };
+
+                    recognizer.SessionStarted += (s, e) =>
+                    {
+                        Console.WriteLine("\nSession started event.");
+                    };
+
+                    recognizer.SessionStopped += (s, e) =>
+                    {
+                        Console.WriteLine("\nSession stopped event.");
+                        Console.WriteLine("\nStop recognition.");
+                        stopRecognition.TrySetResult(0);
+                        Sub.GenerateSrtFile(subs, file);
+                        vttFile = Sub.GenerateWebVTTFile(subs, file);
+                        fileWritten = true;
+                    };
+
+                    // Starts continuous recognition. Uses StopContinuousRecognitionAsync() to stop recognition.
+                    await recognizer.StartContinuousRecognitionAsync().ConfigureAwait(false);
+
+                    // Waits for completion.
+                    // Use Task.WaitAny to keep the task rooted.
+                    Task.WaitAny(new[] { stopRecognition.Task });
+
+                    // Stops recognition.
+                    await recognizer.StopContinuousRecognitionAsync().ConfigureAwait(false);
+                    return vttFile;
+                }
+            }
+            // </recognitionAudioStream>
+        }
+    }
+}
---
+++ TaskEngine/MSTranscription/Sub.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace TaskEngine.MSTranscription
+{
+    public class Sub
+    {
+        public TimeSpan Begin { get; set; }
+        public TimeSpan End { get; set; }
+        public string Caption { get; set; }
+
+        public string SrtSubtitle(int Counter)
+        {
+            String a = "";
+            a += Counter + "\n";
+            a += string.Format("{0:hh\\:mm\\:ss\\,fff} --> {1:hh\\:mm\\:ss\\,fff}\n", Begin, End);
+            a += Caption + "\n\n";
+            return a;
+        }
+
+        public string WebVTTSubtitle(int Counter)
+        {
+            String a = "";
+            a += Counter + "\n";
+            a += string.Format("{0:hh\\:mm\\:ss\\.fff} --> {1:hh\\:mm\\:ss\\.fff}\n", Begin, End);
+            a += Caption + "\n\n";
+            return a;
+        }
+
+        public static List<Sub> GetSubs(Sub sub)
+        {
+            int subLength = 75;
+            List<Sub> subs = new List<Sub>();
+            int length = sub.Caption.Length;
+            string tempCaption = sub.Caption;
+            string caption;
+            int newDuration;
+            TimeSpan curBegin = sub.Begin;
+            TimeSpan curDuration = sub.End.Subtract(sub.Begin);
+            TimeSpan curEnd;
+            while (tempCaption.Length > subLength)
+            {
+                newDuration = Convert.ToInt32(subLength * curDuration.TotalMilliseconds / tempCaption.Length);
+                int index = tempCaption.IndexOf(' ', subLength);
+
+                if (index == -1)
+                {
+                    caption = tempCaption;
+                    tempCaption = "";
+                }
+                else
+                {
+                    caption = tempCaption.Substring(0, index);
+                    tempCaption = tempCaption.Substring(index);
+                    tempCaption = tempCaption.Trim();
+                }
+                curEnd = curBegin.Add(new TimeSpan(0, 0, 0, 0, newDuration));
+                subs.Add(new Sub
+                {
+                    Begin = curBegin,
+                    End = curEnd,
+                    Caption = caption
+                });
+                Console.WriteLine(caption);
+                curBegin = curEnd;
+                curDuration = sub.End.Subtract(curBegin);
+            }
+            if (tempCaption.Length > 0)
+            {
+                newDuration = Convert.ToInt32(subLength * curDuration.TotalMilliseconds / tempCaption.Length);
+                curEnd = curBegin.Add(new TimeSpan(0, 0, 0, 0, newDuration));
+                subs.Add(new Sub
+                {
+                    Begin = curBegin,
+                    End = curEnd,
+                    Caption = tempCaption
+                });
+                Console.WriteLine(tempCaption);
+                curBegin = curEnd;
+                curDuration = sub.End.Subtract(curBegin);
+            }
+            return subs;
+        }
+
+        public static string GenerateSrtFile(List<Sub> subs, String file)
+        {
+            string srtFile = file.Substring(0, file.IndexOf('.')) + ".srt";
+            int Counter = 1;
+            String Subtitle = "";
+            foreach (Sub sub in subs)
+            {
+                Subtitle += sub.SrtSubtitle(Counter++);
+            }
+            WriteSubtitleToFile(Subtitle, srtFile);
+            return srtFile;
+        }
+
+        public static string GenerateWebVTTFile(List<Sub> subs, String file)
+        {
+            string srtFile = file.Substring(0, file.IndexOf('.')) + ".vtt";
+            int Counter = 1;
+            String Subtitle = "WEBVTT\nKind: subtitles\nLanguage: en\n\n";
+            foreach (Sub sub in subs)
+            {
+                Subtitle += sub.WebVTTSubtitle(Counter++);
+            }
+            WriteSubtitleToFile(Subtitle, srtFile);
+            return srtFile;
+        }
+
+        public static void WriteSubtitleToFile(string Subtitle, string srtFile)
+        {
+            try
+            {
+
+                //Pass the filepath and filename to the StreamWriter Constructor
+                StreamWriter sw = new StreamWriter(srtFile, false, System.Text.Encoding.UTF8);
+                //Write a line of text
+                sw.WriteLine(Subtitle);
+                //Close the file
+                sw.Close();
+            }
+            catch (Exception e1)
+            {
+                Console.WriteLine("Exception: " + e1.Message);
+            }
+        }
+    }
+}
