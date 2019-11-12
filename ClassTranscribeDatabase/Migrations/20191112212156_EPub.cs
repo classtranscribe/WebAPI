9a58525b57cfd54d3490205a558be82bbc7aec7e
Lawrence Angrave <angrave@illinois.edu>
2019-11-12 15:39:33 -0600

    Added Epub migration

---
+++ ClassTranscribeDatabase/Migrations/20191112212156_EPub.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ClassTranscribeDatabase.Migrations
+{
+    public partial class EPub : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EPub",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    LastUpdatedAt = table.Column<DateTime>(nullable: false),
+                    LastUpdatedBy = table.Column<string>(nullable: true),
+                    IsDeletedStatus = table.Column<int>(nullable: false),
+                    Language = table.Column<string>(nullable: true),
+                    FileId = table.Column<string>(nullable: true),
+                    VideoId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EPub", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EPub_FileRecords_FileId",
+                        column: x => x.FileId,
+                        principalTable: "FileRecords",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_EPub_Videos_VideoId",
+                        column: x => x.VideoId,
+                        principalTable: "Videos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EPub_FileId",
+                table: "EPub",
+                column: "FileId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EPub_VideoId",
+                table: "EPub",
+                column: "VideoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EPub");
+        }
+    }
+}
