ffc70bc26471140ceee305d0a727efa81d84143b
Chirantan Mahipal <chirantan31@gmail.com>
2020-03-09 10:13:27 -0500

    SlackLogger bug

--- ClassTranscribeDatabase/Slack.cs
+++ ClassTranscribeDatabase/Slack.cs
@@ -29,7 +29,7 @@ namespace ClassTranscribeDatabase
         {
             var obj = new JObject();
             obj["message"] = message;
-            obj["Exception"] = JObject.FromObject(e.Message);
+            obj["Exception"] = e.Message;
 
             await PostMessageAsync(obj);
         }
