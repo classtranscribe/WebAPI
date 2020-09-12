aa2954e1ab3bf33a592aa7cac9ca7fac38af93b6
Lawrence Angrave <angrave@illinois.edu>
2020-09-12 17:10:28 -0500

    Revert "Ensure sane CPU load by limiting ffmpeg to a configurable number of CPU cores."
    This reverts commit 1f258c6a326ada13ad8f89911a8459a3514e8486.

--- PythonRpcServer/ffmpeg.py
+++ PythonRpcServer/ffmpeg.py
@@ -1,26 +1,20 @@
 from ffmpy import FFmpeg
+from utils import getTmpFile
 from time import perf_counter 
-
-import utils
-
-default_max_threads = 3
 
 def convertVideoToWavWithOffset(input_filepath, offset):
     try:
         start_time = perf_counter()
         if offset is None:
             offset = 0.0
-        
-        nthreads = utils.getMaxThreads()
-        
-        print(f"convertVideoToWavWithOffset('{input_filepath}',{offset}) using {nthreads} thread(s).")
-        output_filepath = utils.getTmpFile()
+        print(f"convertVideoToWavWithOffset({input_filepath},{offset})")
+        output_filepath = getTmpFile()
         # For less verbosity try, global_options= '-hide_banner -loglevel error -nostats'
         # See https://github.com/Ch00k/ffmpy/blob/master/ffmpy.py
 
         ext = '.wav'
         ff = FFmpeg(
-            global_options=f"-hide_banner -loglevel error -nostats -threads {nthreads}",
+            global_options='-hide_banner -loglevel error -nostats',
             inputs={
                 input_filepath: '-ss {}'.format(offset)},
             outputs={output_filepath: '-c:a pcm_s16le -ac 1 -y -ar 16000 -f wav'}
@@ -28,31 +22,28 @@ def convertVideoToWavWithOffset(input_filepath, offset):
         print(f"Starting. Audio output will be saved in {output_filepath}")
         ff.run()
         end_time = perf_counter()
-        print(f"convertVideoToWavWithOffset('{input_filepath}',{offset}) Complete. Duration {int(end_time - start_time)} seconds")
+        print(f"convertVideoToWavWithOffset({input_filepath},{offset}) Complete. Duration {int(end_time - start_time)} seconds")
         return output_filepath, ext
     except Exception as e:
         print("Exception:" + str(e))
         raise e
 
-# Creates a low res mp4
+
 def processVideo(input_filepath):
     try:
         start_time = perf_counter()
-
-        nthreads = utils.getMaxThreads()
-
-        print(f"processVideo('{input_filepath}') using {nthreads} threads")
-        output_filepath = utils.getTmpFile()
+        print(f"processVideo({input_filepath})")
+        output_filepath = getTmpFile()
         ext = '.mp4'
         ff = FFmpeg(
-            global_options= f"-hide_banner -loglevel error -nostats -threads {nthreads}",
+            global_options='-hide_banner -loglevel error -nostats',
             inputs={input_filepath: None},
             outputs={
                 output_filepath: '-c:v libx264 -f mp4 -b:v 500K -s 768x432 -movflags faststart -ar 48000 -preset medium'}
         )
         ff.run()
         end_time = perf_counter()
-        print(f"processVideo('{input_filepath}') Complete. Duration {int(end_time - start_time)} seconds")
+        print(f"processVideo({input_filepath}) Complete. Duration {int(end_time - start_time)} seconds")
         return output_filepath, ext
     except Exception as e:
         print("Exception:" + str(e))
--- PythonRpcServer/utils.py
+++ PythonRpcServer/utils.py
@@ -7,64 +7,20 @@ import os
 import requests
 import mimetypes
 
-## CAUTION ##
-# When imported this file seeds the RNG with random bytes from the os.urandom() - see below
-
-# Max number of threads this process should use. Be careful about setting this value too high
-# Unless you enjoy bringing a production machine to a crawl and debugging timeouts
-# Recommended values 1-4
-DEFAULT_JOB_MAX_THREAD = 4
-
 DATA_DIRECTORY = os.getenv('DATA_DIRECTORY')
-
-
-# Returns the preferred maximum number of threads to use
-def getMaxThreads():
-    return int(os.getenv('JOB_MAX_THREADS', DEFAULT_JOB_MAX_THREAD))
-
 
 def encode(obj):
     return codecs.encode(pickle.dumps(obj), "base64").decode()
-
-
 def decode(pickled):
     return pickle.loads(codecs.decode(pickled.encode(), "base64"))
 
-
-# We could add lowercase, but since this is used for filenames and some filesystems are case insensitve, so let's not.
-# Excluding 1 and 0 (too visually to similar to IO)
 def getRandomString(n):
-    return ''.join(random.choices(string.ascii_uppercase + "23456789", k=n))
-
-
-# Ensure two python processes started at the same time, are seeded with different values
-# reduces likelihood of two random filename collision
-random.seed(os.urandom(8))
-
-# Returns a path to a file that does not exit.
-# There is a use-after-check race condition that two processes might randomly choose the same file path
-# And the could be greater than the improbably 2 in 34^12 if two process RNG were identically seeded
-# e.g. were seeded by an identical time.
-# Thus each process pulls bytes from os.urandom to see python's RNG
-# See random.seed - in this file
-
+    return ''.join(random.choices(string.ascii_uppercase + string.digits, k=n))
 
 def getTmpFile():
-    while True:
-        # A key space of 34^12 should be sufficient for us...
-        filenameSize = 12
-        candidate = os.path.join(DATA_DIRECTORY, getRandomString(filenameSize))
-        if not os.path.exists(candidate):
-            return candidate
-        # We wil never print this, and if we do, no-one will read it.
-        # The loop exists purely for reasoning about the code
-        print("This loop so precious; Unrun unlogged yet forces; The bug is elsewhere.")
+    return os.path.join(DATA_DIRECTORY, getRandomString(8))
 
-# Downloads given URL to the specified filepath (or if unspecified, a random filename)
-# Filepath and cookies may be specified
-# Returns a two tuple, [filepath,  extension]
-# An appropriate Extension is guessed based on the mimetype in the 'content-type' response header
-def download_file(url, filepath=None, cookies=None):
+def download_file(url, filepath = None, cookies = None):
     # NOTE the stream=True parameter below
     if not filepath:
         filepath = getTmpFile()
@@ -73,8 +29,8 @@ def download_file(url, filepath=None, cookies=None):
         extension = mimetypes.guess_extension(r.headers['content-type'])
         r.raise_for_status()
         with open(filepath, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=8192):
-                if chunk:  # filter out keep-alive new chunks
+            for chunk in r.iter_content(chunk_size=8192): 
+                if chunk: # filter out keep-alive new chunks
                     f.write(chunk)
                     # f.flush()
     return filepath, extension
