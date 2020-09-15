9956fa056b9af2c48819f4050a08b6dd4a46c74b
Feng <fengruoyi97@gmail.com>
2020-09-15 06:34:49 -0500

    add clientSDK

---
+++ ClientSdk/ClientFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientSdk.Client
+{
+    public class ClientFactory
+    {
+        public static MyClient CreateTestClient(string baseUrl, HttpClient http, string _token)
+        {
+            return new MyClient(baseUrl, http)
+            {
+                token = _token
+            };
+        }
+    }
+}
