3c343a0da3091edaed60ea5a1103a9d721701ca8
Feng <fengruoyi97@gmail.com>
2020-09-23 06:05:11 -0500

    add simple api test framework

---
+++ ApiTest/AssertInfo.cs
@@ -0,0 +1,86 @@
+﻿using JsonDiffPatchDotNet;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+
+namespace Tests
+{
+    public class AssertInfo
+    {
+        public string assert;
+        public string expect;
+        public string output;      
+        public string requestBody;
+        public string diffInfo;
+
+        public static AssertInfo SingleCaseAssert(string expectation, dynamic result, List<string> input, string api, string method)
+        {
+
+            AssertInfo ret = new AssertInfo()
+            {
+                expect = expectation,
+                output = JsonConvert.SerializeObject(result, Formatting.Indented),
+                requestBody = JsonConvert.SerializeObject(new
+                {
+                    api,
+                    method,
+                    input
+                }, Formatting.Indented)
+            };
+            
+            if (result == null && expectation == null)
+            {
+                ret.assert = "succeed";
+                return ret;
+            }
+            if (result == null || expectation == null)
+            {
+                ret.assert = "fail";
+                ret.diffInfo = "";
+                return ret;
+            }
+
+            //schema matching 
+            //JSchemaGenerator generator = new JSchemaGenerator();
+            //generator.SchemaIdGenerationHandling = SchemaIdGenerationHandling.TypeName;
+            //Console.WriteLine(result[0].GetType());
+            //JSchema schema = generator.Generate(typeof(Contracts.Offering));
+            //string jsonSchema = schema.ToString();
+
+
+            //content matching
+            JsonSerializer jsonSerializer = JsonSerializer.Create(Setting.jsonSetting); 
+            JToken exp = JToken.FromObject(JsonConvert.DeserializeObject(expectation, result.GetType(), Setting.jsonSetting), jsonSerializer);
+            JToken output = JToken.FromObject(result, jsonSerializer);
+
+            JToken patch = Utils.JsonDeepCompare(exp, output);
+            ret.diffInfo = patch?.ToString();
+            ret.assert = patch == null? "succeed" : "fail";
+            
+            Console.WriteLine(ret.diffInfo);
+            return ret;
+        }
+
+
+
+        public static List<AssertInfo> MultipleCaseAssert(List<string> expectations, List<dynamic> results, List<List<string>> inputs, string api, string method)
+        {
+           
+            List<AssertInfo> rets = new List<AssertInfo>();
+
+            for (int i = 0; i < results.Count; i++)
+            {
+                rets.Add(SingleCaseAssert(expectations[i], results[i], inputs[i], api, method));
+            }
+
+            return rets;
+        }
+
+    }
+
+}
---
+++ ApiTest/Utils.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JsonDiffPatchDotNet;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Globalization;
+
+namespace Tests
+{
+
+    class Utils
+    {
+
+        public static string toTitleCase(string str)
+        {
+            TextInfo myTI = new CultureInfo("en-US", false).TextInfo;
+
+            return myTI.ToTitleCase(str);
+        }
+        
+        // Parse string into required type
+        public static dynamic SingleParamParse(string obj, Type t)
+        {
+            if (obj == null)
+            {
+                return null;
+            }
+            if (t == typeof(string))
+            {
+                return obj;
+            }
+            if (t == typeof(DateTime))
+            {
+                return DateTime.Parse(obj);
+            }
+
+            // if parameter type is Object
+            JToken jtoken = JToken.Parse(obj);
+
+            dynamic rets = jtoken.ToObject(t);
+
+            return rets;
+
+        }
+
+        public static List<dynamic> MultipleParamParse(List<string> objs, List<Type> types)
+        {
+            List<dynamic> ret = new List<dynamic>();
+            for (int i = 0; i < types.Count; i++)
+            {
+                ret.Add(SingleParamParse(objs[i], types[i]));
+            }
+            return ret;
+        }
+
+        // compare two jsons, able to compare two JArray with the same content but different order
+        public static JToken JsonDeepCompare(JToken left, JToken right)
+        {
+            var jdp = new JsonDiffPatch();
+
+            JToken patch = jdp.Diff(left, right);
+
+            return patch;
+        }
+
+        
+    }
+
+}
