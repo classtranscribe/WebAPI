0cb541685bfa348a6c29b1c031bc7f0e5e78c984
Lawrence Angrave <angrave@illinois.edu>
2020-09-23 21:27:54 -0500

    Use GUID as random number source

--- ClassTranscribeDatabase/CommonUtils.cs
+++ ClassTranscribeDatabase/CommonUtils.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json;
+using RabbitMQ.Client.Content;
 using System;
 using System.IO;
 using System.Linq;
@@ -13,21 +14,21 @@ namespace ClassTranscribeDatabase
         // and we dont want the meaning of existing entries to change due to future modifications of this list
         public enum TaskType
         {
-            PeriodicCheck =1,
-            DownloadAllPlaylists=2,
-            DownloadPlaylistInfo=3,
-            DownloadMedia=4,
-            ConvertMedia=5,
-            Transcribe=6,
-            ProcessVideo=7,
-            Aggregator=8,
-            GenerateVTTFile=9,
-            QueueAwaker=10,
-            SceneDetection=11,
-            UpdateBoxToken=12,
-            CreateBoxToken=13,
-            UpdateOffering=14,
-            ReTranscribePlaylist=15
+            PeriodicCheck = 1,
+            DownloadAllPlaylists = 2,
+            DownloadPlaylistInfo = 3,
+            DownloadMedia = 4,
+            ConvertMedia = 5,
+            Transcribe = 6,
+            ProcessVideo = 7,
+            Aggregator = 8,
+            GenerateVTTFile = 9,
+            QueueAwaker = 10,
+            SceneDetection = 11,
+            UpdateBoxToken = 12,
+            CreateBoxToken = 13,
+            UpdateOffering = 14,
+            ReTranscribePlaylist = 15
         }
 
         public class Languages
@@ -55,54 +56,52 @@ namespace ClassTranscribeDatabase
         private static int seed; // member var only for future debugging /replay options
         private static Random random;
         /// <summary>Ensures we have a well seeded RNG even if two processes or two threads  are started at the same time</summary>    
-        private static Random getSharedRandom()
-        {
-            if (random != null) 
-                return random; // Early return
-            // Use something external and trust worthy if it exists
-            const string randomStream = "/dev/urandom";
-            if (File.Exists(randomStream)) {
-                using (BinaryReader reader = new BinaryReader(File.Open(randomStream, FileMode.Open)))
-                {
-                    seed = reader.ReadInt32();
-                }
-            } else
-            {
-                // Fallback
-                seed = Guid.NewGuid().ToString().GetHashCode();
-            }
-            // TODO/REVIEW:Random ctor seed is limited to 32 bits!
-            // Eventually we will need something better
-            random = new Random(seed);
-            return random;
-        }
+
         public static string RandomString(int length)
         {
             // TODO/TOREVIEW: C# random is seeded with a time since 1970 at 100ns, so what if two
             // threads start at the same time, to within the tick resolution?
             // e.g. https://stackoverflow.com/questions/1785744/how-do-i-seed-a-random-class-to-avoid-getting-duplicate-random-values#
             // Could this mean that two threads cmight be seeded with the same number
-            Random random = getSharedRandom();
+
             // Some filesystems are case insensitive so only use uppercase
             // Drop 1 and 0 because they are too similar to IO
             const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ23456789";
-            string result;
-            lock (random)
+
+            // The day we want GUID-like filenames we can return Guid.NewGuid().ToString()
+            // But for now there are too many GUIDS when debugging data, so let's keep filename as not GUID like
+
+            // This new GUID as source of random bytes implementation avoids a race file lock in /dev/urandom
+            // To understand the next line, imagine chars.length was 100. We would only want the values 0 - 199
+            int maxFair = 255 - (255 % chars.Length);
+
+            StringBuilder result = new StringBuilder(length);
+            while (true)
             {
-                 result = new string(Enumerable.Repeat(chars, length)
-                  .Select(s => s[random.Next(s.Length)]).ToArray());
+                foreach (byte b in Guid.NewGuid().ToByteArray())
+                {
+                    if (b < maxFair)
+                    {
+                        result.Append( chars[b % chars.Length]);
+                        if (result.Length == length)
+                        {
+                            return result.ToString();
+                        }
+                    }
+
+                }
             }
-            return result;
         }
 
         public static string GetTmpFile()
         {
             // Align with python code.
             const int filenaNmeLength = 12; // was 8
-            while(true) {
+            while (true)
+            {
                 string candidate = Path.Combine(Globals.appSettings.DATA_DIRECTORY, RandomString(filenaNmeLength));
-                if( ! File.Exists(candidate)) return candidate;
-            }   
+                if (!File.Exists(candidate)) return candidate;
+            }
         }
     }
 }
