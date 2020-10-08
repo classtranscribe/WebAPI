21d10cfc71085a4e5b1212ae94b2a06f815d5433
Lawrence Angrave <angrave@illinois.edu>
2020-10-08 12:18:35 -0500

    Video add FileMediaInfo and Duration properties

---
+++ ClassTranscribeDatabase/Migrations/20201008171703_AddDurationFileMediaInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ClassTranscribeDatabase.Migrations
+{
+    public partial class AddDurationFileMediaInfo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "Duration",
+                table: "Videos",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FileMediaInfo",
+                table: "Videos",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Duration",
+                table: "Videos");
+
+            migrationBuilder.DropColumn(
+                name: "FileMediaInfo",
+                table: "Videos");
+        }
+    }
+}
