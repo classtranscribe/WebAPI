0382f4b7046fc3f9930971f7522845425eb459fc
wenyugu2 <wenyugu2@illinois.edu>
2020-11-16 17:22:38 -0600

    Added todo

--- ClassTranscribeServer/Controllers/CaptionsSearchController.cs
+++ ClassTranscribeServer/Controllers/CaptionsSearchController.cs
@@ -26,6 +26,7 @@ namespace ClassTranscribeServer.Controllers
         [HttpPost]
         public async Task<ActionResult<Caption>> Search([FromBody] string[] ids, string query, int page = 1, int pageSize = 10)
         {
+            // TODO: add authentication
             if (ids == null || ids.Length == 0)
             {
 
