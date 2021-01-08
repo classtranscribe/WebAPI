69384d19f693b83524026adbb801c096cebd66fd
Houyuan Sha <houyuan2@illinois.edu>
2021-01-07 18:54:49 -0600

    Changes per comments

--- TaskEngine/Tasks/CleanUpElasticIndexTask.cs
+++ TaskEngine/Tasks/CleanUpElasticIndexTask.cs
@@ -45,44 +45,59 @@ namespace TaskEngine.Tasks
             var indices = result.Indices;
             foreach (var index in indices)
             {
-                string index_name = index.Key.ToString();
-
-                // elastic search internal management index starts with '.'
-                if (index_name[0] == '.')
+                try
                 {
+                    string index_name = index.Key.ToString();
+
+                    // elastic search internal management index starts with '.'
+                    if (index_name[0] == '.')
+                    {
+                        continue;
+                    }
+
+                    GetLogger().LogInformation("Running Index: " + index_name);
+                    string[] parts = index_name.Split("_");
+
+                    if (parts.Length != 3)
+                    {
+                        GetLogger().LogError("Parsing Error: invalid Index Name");
+                        continue;
+                    }
+
+                    var index_string_id = parts[0];
+                    var index_string_lang = parts[1];
+                    var index_string_time = parts[2];
+
+                    // Indices that were created in the last 2 days would not be removed.
+                    DateTime createdAt = DateTime.ParseExact(index_string_time, "yyyyMMddHHmmss", null);
+                    GetLogger().LogInformation("Created at: " + createdAt.ToString());
+                    DateTime range = DateTime.Now.AddMinutes(-_time_to_live);
+                    if (DateTime.Compare(createdAt, range) >= 0)
+                    {
+                        GetLogger().LogInformation("Skipped: Index does not exceed time_to_live");
+                        continue;
+                    }
+
+                    // If alias exist, this index is currently in use and should not be removed.
+                    var index_string_alias = index_string_id + "_" + index_string_lang + "_" + "primary";
+                    var cur_index = await _client.GetIndicesPointingToAliasAsync(index_string_alias);
+                    var cur_index_list = cur_index.ToList();
+                    if (cur_index_list.Any() && cur_index_list[0].ToString() == index_name)
+                    {
+                        GetLogger().LogInformation("Skipped: Index is in use");
+                        continue;
+                    }
+
+                    // remove index
+                    GetLogger().LogInformation("Removing Index: " + index_name);
+                    var resp = _client.Indices.Delete(index_name);
+                    GetLogger().LogInformation(resp.ToString());
+                }
+                catch (Exception e)
+                {
+                    GetLogger().LogError("Error: " + e.ToString());
                     continue;
                 }
-
-                GetLogger().LogInformation(index.Key.ToString());
-                string[] parts = index_name.Split("_");
-                var index_string_id = parts[0];
-                var index_string_lang = parts[1];
-                var index_string_time = parts[2];
-
-                // Indices that were created in the last 2 days would not be removed.
-                DateTime createdAt = DateTime.ParseExact(index_string_time, "yyyyMMddHHmmss", null);
-                GetLogger().LogInformation("Created at: " + createdAt.ToString());
-                DateTime range = DateTime.Now.AddMinutes(-_time_to_live);
-                if (DateTime.Compare(createdAt, range) >= 0)
-                {
-                    GetLogger().LogInformation("Skipped: Index does not exceed time_to_live");
-                    continue;
-                }
-
-                // If alias exist, this index is currently in use and should not be removed.
-                var index_string_alias = index_string_id + "_" + index_string_lang + "_" + "primary";
-                var cur_index = await _client.GetIndicesPointingToAliasAsync(index_string_alias);
-                var cur_index_list = cur_index.ToList();
-                if (cur_index_list.Any() && cur_index_list[0].ToString() == index_name)
-                {
-                    GetLogger().LogInformation("Skipped: Index is in use");
-                    continue;
-                }
-
-                // remove index
-                var resp = _client.Indices.DeleteAsync(index_name);
-                GetLogger().LogInformation(resp.Result.ToString());
-                GetLogger().LogInformation("Removed");
             }
             GetLogger().LogInformation("CleanUpElasticIndexTask Done");
         }
