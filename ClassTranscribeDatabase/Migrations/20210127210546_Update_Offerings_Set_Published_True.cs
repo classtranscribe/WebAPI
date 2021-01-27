9d736facfcffc52cf193514b7cc8bfab83fb5a37
Yevhen <yevhen5v@gmail.com>
2021-01-27 23:20:47 +0200

    migration for making existing offerings published

---
+++ ClassTranscribeDatabase/Migrations/20210127210546_Update_Offerings_Set_Published_True.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ClassTranscribeDatabase.Migrations
+{
+    public partial class Update_Offerings_Set_Published_True : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "UPDATE \"Offerings\" " +
+                "SET \"PublishStatus\" = 1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
