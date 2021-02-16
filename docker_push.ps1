e5d3a0feb1c1b93d4a09675db56c627e96ce34ad
Lawrence Angrave <angrave@illinois.edu>
2021-02-16 12:13:18 -0600

    Delete docker_push.ps1
    No longer required

--- docker_push.ps1
+++
@@ -1,6 +0,0 @@
-docker build -f .\pythonrpcserver.Dockerfile -t classtranscribe/pythonrpcserver:$1 .
-docker push classtranscribe/pythonrpcserver:$1
-docker build -f .\API.Dockerfile -t classtranscribe/api:$1 .
-docker push classtranscribe/api:$1
-docker build -f .\TaskEngine.Dockerfile -t classtranscribe/taskengine:$1 .
-docker push classtranscribe/taskengine:$1
