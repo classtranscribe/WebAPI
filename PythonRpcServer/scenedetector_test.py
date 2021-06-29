5ed8d56690d693aba7ab6fd56261612b450a2022
lijiaxi2018 <jiaxili3@illinois.edu>
2021-06-29 03:22:34 -0500

    Added more tests, Simplified testing structure

--- PythonRpcServer/scenedetector_test.py
+++ PythonRpcServer/scenedetector_test.py
@@ -1,23 +1,22 @@
 import scenedetector as sd
 import time
-import os
 import json
-import pytube
+import os
+import urllib.request
+import shutil
 
 DATA_DIR = os.getcwd()
 
-def test_simple_lecture_video():
-    # toy_lecture.mp4
-    print("----------test_simple_lecture_video---STARTED----------")
+# General Testing Scheme For All Test Cases
+def test_scheme(folder_name, url, expected_phrases):
+    print("----------" + folder_name + "---STARTED----------")
+
+    video_name = folder_name + '.mp4'
+    video_path = DATA_DIR + '/' + video_name
+    folder_path = DATA_DIR + '/' + folder_name
 
     # Download the video file
-    toy_lecture_url = 'https://www.youtube.com/watch?v=nF8c9kAtGVU'
-    toy_lecture_file = pytube.YouTube(toy_lecture_url)
-    toy_lecture_video = toy_lecture_file.streams.first()
-    toy_lecture_video.download()
-
-    video_name = toy_lecture_video.title + '.mp4'
-    video_path = DATA_DIR + '/' + toy_lecture_video.title + '.mp4'
+    urllib.request.urlretrieve(url, video_name) 
 
     # Run SceneDetector on the video 
     toy_lecture_json = sd.find_scenes(video_name)
@@ -30,20 +29,30 @@ def test_simple_lecture_video():
     
     raw_phrases = '\n'.join( ['\n'.join(words) for words in corpus] )
     raw_phrases = raw_phrases.replace('``','')
-    print('raw_phrases', raw_phrases)
 
     # Check phrase occurance
-    expected_phrases = ['Slide One', 'Slide Two', 'Slide Three']
     for phrase in expected_phrases:
         assert(phrase in raw_phrases)
+        print("Phrase " + phrase + " was the OCR output")
 
-    # Delete the file
+    # Delete files
     os.remove(video_path)
+    shutil.rmtree(folder_path)
 
-    print("----------test_simple_lecture_video---PASSED----------")
+    print("----------" + folder_name + "---Passed----------")
 
 def run_scenedetector_tests():
-	test_simple_lecture_video()
+    video_names = ['test_1_toy_lecture', 'test_2_241_thread']
+    urls = [
+        'https://app.box.com/index.php?rm=box_download_shared_file&shared_name=pec6m3vbjzu2l4d2m1gv9588npq9nw7o&file_id=f_827175578540',
+        'https://app.box.com/index.php?rm=box_download_shared_file&shared_name=xbs7aqcsnrjdglplc6neh3fxdhkqz2fi&file_id=f_827616401395'
+    ]
+    expected_phrases_list = [
+        ['Slide One', 'float', 'int'],
+        ['pthread_create', 'Compile', 'stacks', 'printf', 'nothing happens']
+    ]
+    for i in range(len(video_names)):
+        test_scheme(video_names[i], urls[i], expected_phrases_list[i])
 
 if __name__ == '__main__': 
 	run_scenedetector_tests();
