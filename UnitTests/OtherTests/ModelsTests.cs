0df12dd28c1d207122801c0dc5a7ff50d6342860
Asher Dale <asher@dales.org>
2021-09-08 15:24:19 -0400

    First add unit tests to show that they fail without the change

---
+++ UnitTests/OtherTests/ModelsTests.cs
@@ -0,0 +1,17 @@
+﻿using ClassTranscribeDatabase.Models;
+using Xunit;
+
+namespace UnitTests.OtherTests
+{
+    public class ModelsTests
+    {
+        [Fact]
+        public void Video_JObjects_Not_Null()
+        {
+            var video = new Video();
+            Assert.NotNull(video.JsonMetadata);
+            Assert.NotNull(video.SceneData);
+            Assert.NotNull(video.FileMediaInfo);
+        }
+    }
+}
