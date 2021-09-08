6d5329ce756aa01ca26253c278c69d223e64678e
Asher Dale <asher@dales.org>
2021-09-08 15:37:54 -0400

    Add more specific unit test for testing empty JObjects

--- UnitTests/OtherTests/ModelsTests.cs
+++ UnitTests/OtherTests/ModelsTests.cs
@@ -9,9 +9,14 @@ namespace UnitTests.OtherTests
         public void Video_JObjects_Not_Null()
         {
             var video = new Video();
+
             Assert.NotNull(video.JsonMetadata);
             Assert.NotNull(video.SceneData);
             Assert.NotNull(video.FileMediaInfo);
+
+            Assert.Equal("{}", video.JsonMetadata.ToString());
+            Assert.Equal("{}", video.SceneData.ToString());
+            Assert.Equal("{}", video.FileMediaInfo.ToString());
         }
     }
 }
