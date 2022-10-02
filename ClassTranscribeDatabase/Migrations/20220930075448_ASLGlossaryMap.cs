b911c98119d844c52e5dad1c1650be5c022fe62f
Jiaxi Li <ljx@MacBook-Pro-439.local>
2022-10-02 03:10:10 -0500

    DB: Update Glossary, Video, Added ASLVideo, ASLGlossaryMap

---
+++ ClassTranscribeDatabase/Migrations/20220930075448_ASLGlossaryMap.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ClassTranscribeDatabase.Migrations
+{
+    public partial class ASLGlossaryMap : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ASLVideoGlossaryMaps",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: false),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    LastUpdatedAt = table.Column<DateTime>(nullable: false),
+                    LastUpdatedBy = table.Column<string>(nullable: true),
+                    IsDeletedStatus = table.Column<int>(nullable: false),
+                    DeletedAt = table.Column<DateTime>(nullable: true),
+                    DeletedBy = table.Column<string>(nullable: true),
+                    GlossaryId = table.Column<string>(nullable: true),
+                    ASLVideoId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ASLVideoGlossaryMaps", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ASLVideoGlossaryMaps");
+        }
+    }
+}
