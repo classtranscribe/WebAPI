6a6576a5e1b89853d4c9c08684dc7c18fe0c6208
Jiaxi Li <ljx@MacBook-Pro-607.local>
2022-11-29 22:35:01 -0600

    Updated Glossary Controllers and Save Glossary to the TextData

---
+++ ClassTranscribeDatabase/Migrations/20221129095657_GlossaryDataId.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ClassTranscribeDatabase.Migrations
+{
+    public partial class GlossaryDataId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "GlossaryDataId",
+                table: "Videos",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GlossaryDataId",
+                table: "Videos");
+        }
+    }
+}
