3d86b314e89e289f74004f9b2427e550bcc3ae56
Lawrence Angrave <angrave@illinois.edu>
2024-03-01 13:40:31 -0600

    UpdateBuild

--- install-speech-hack-libssl1.sh
+++ install-speech-hack-libssl1.sh
@@ -3,18 +3,21 @@
 # Temporary 2024 Hack for MS SpeechSDK to run on dotnet8
 # https://github.com/Azure-Samples/cognitive-services-speech-sdk/issues/2204
 
-ARCH=$(dpkg --print-architecture)
+SSLVERSION="1.1.1f"
 
+ARCH=$(dpkg --print-architecture)
 if [ "$ARCH" = "arm64" ] ; then
-    BASE="http://ports.ubuntu.com/ubuntu-ports/pool/main/o/openssl/"
+    BASE="http://ports.ubuntu.com/pool/main/o/openssl"
+    RELEASE="1ubuntu2"
 else
-    BASE="http://security.ubuntu.com/ubuntu/pool/main/o/openssl/"
+    BASE="http://security.ubuntu.com/ubuntu/pool/main/o/openssl"
+    RELEASE="1ubuntu2"
 fi
 
-wget $BASE/libssl1.1_1.1.1f-1ubuntu2.20_${ARCH}.deb
-wget $BASE/libssl-dev_1.1.1f-1ubuntu2.20_${ARCH}.deb 
-dpkg -i libssl1.1_1.1.1f-1ubuntu2.20_${ARCH}.deb 
-dpkg -i libssl-dev_1.1.1f-1ubuntu2.20_${ARCH}.deb
-rm libssl1.1_1.1.1f-1ubuntu2.20_${ARCH}.deb libssl-dev_1.1.1f-1ubuntu2.20_${ARCH}.deb
+wget ${BASE}/libssl1.1_${SSLVERSION}-${RELEASE}_${ARCH}.deb
+wget ${BASE}/libssl-dev_${SSLVERSION}-${RELEASE}_${ARCH}.deb 
+dpkg -i libssl1.1_${SSLVERSION}-${RELEASE}_${ARCH}.deb 
+dpkg -i libssl-dev_${SSLVERSION}-${RELEASE}_${ARCH}.deb
+rm libssl1.1_${SSLVERSION}-${RELEASE}_${ARCH}.deb libssl-dev_${SSLVERSION}-${RELEASE}_${ARCH}.deb
 
 
